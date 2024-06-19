dab73ef4065a72616be56967356ff1929ed1ce3c
Rishi2705 <rishiarora2705@gmail.com>
2024-06-19 15:04:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myweatherapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myweatherapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val search = findViewById<CardView>(R.id.search)
+
+    }
+}
